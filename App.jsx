0323404011ad1f2b2ee21927ef66f3677168803d
Munevera Plojovic <plojovicm3@gmail.com>
2023-01-18 22:57:41 +0100

    useState and useEffect hooks

--- App.jsx
+++ App.jsx
@@ -2,14 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 import StyledButton from "./components/styledButton/StyledButton";
 import StyledHeader from "./components/styledHeader/StyledHeader";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [innerText, setInnerText] = useState("");
+  const [clicked, setClicked] = useState(false);
+
+  useEffect(() => {
+    clicked ? setInnerText("Clicked") : setInnerText("Click me");
+  }, [clicked]);
+
+  function buttonClick() {
+    setClicked(!clicked);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <StyledHeader innerHTML="First React app!" />
-        <StyledButton innerText="Click me" />
+        <StyledButton innerText={innerText} onClickHandler={buttonClick} />
         <a
           className="App-link"
           href="https://reactjs.org"
