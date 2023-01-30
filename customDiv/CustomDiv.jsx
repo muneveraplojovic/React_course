a047322835549b3634b3aef1aa2ca7381ac1b335
Munevera Plojovic <plojovicm3@gmail.com>
2023-01-30 20:50:57 +0100

    customizable css

--- customDiv/CustomDiv.jsx
+++ customDiv/CustomDiv.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const CustomDiv = ({ children }) => {
-  return <div>{children}</div>;
+const CustomDiv = ({ children, width, height, borderRadius, border }) => {
+  const divWidth = width ? width : 100;
+  const divHeight = height ? height : 100;
+  const divBr = borderRadius ? borderRadius : 0;
+  const divBorder = border ? border : "1px solid black";
+
+  return (
+    <div
+      style={{
+        width: divWidth,
+        height: divHeight,
+        border: divBorder,
+        borderRadius: divBr,
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default CustomDiv;
