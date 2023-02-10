48c3a6aedc6d215deb5eb29b02b88f048af55575
Munevera Plojovic <plojovicm3@gmail.com>
2023-02-10 23:49:45 +0100

    ...

--- Templates/Navigation/Navigation.jsx
+++ Templates/Navigation/Navigation.jsx
@@ -1,13 +1,11 @@
 import { Grid } from "@mui/material";
 import React from "react";
-import CustomDiv from "../../components/customDiv/CustomDiv";
+import TopRow from "./TopRow";
 
 const Navigation = () => {
   return (
     <Grid container direction="row">
-      <CustomDiv padding="0px 20px" border="1px solid white" width="100%">
-        <Grid container direction="row"></Grid>
-      </CustomDiv>  
+        <TopRow />
     </Grid>
   );
 };
--- Templates/Navigation/TopRow.jsx
+++ Templates/Navigation/TopRow.jsx
@@ -1,8 +1,36 @@
 import React from "react";
 import { Grid } from "@mui/material";
+import CustomDiv from "../../components/customDiv/CustomDiv";
+import { colors, fontSize } from "../../util/theme";
+import PhoneIcon from "@mui/icons-material/Phone";
+//import { EmailIcon } from "@mui/icons-material";
 
 const TopRow = () => {
-  return <Grid container direction="row"></Grid>;
+  return (
+    <CustomDiv
+      bgColor={colors.secondColor}
+      width="100%"
+      height="auto"
+      padding="0px 5%"
+      border="0px"
+    >
+      <Grid container direction="row">
+        <Grid item md={6} lg={6} spacing={0}>
+          <CustomDiv display="flex" alignItems="center">
+            <PhoneIcon
+              style={{
+                fontSize: fontSize.normal,
+              }}
+            />
+            <p>+3816333333</p>
+          </CustomDiv>
+        </Grid>
+        <Grid item md={6} lg={6} spacing={0}>
+          <CustomDiv width="50%" border="0px" bgColor="inherit"></CustomDiv>
+        </Grid>
+      </Grid>
+    </CustomDiv>
+  );
 };
 
 export default TopRow;
