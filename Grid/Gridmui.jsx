947a87edc507c495f873495961eddde85b4a8cf7
Munevera Plojovic <plojovicm3@gmail.com>
2023-02-15 23:49:32 +0100

    grid mui components

--- Grid/Gridmui.jsx
+++ Grid/Gridmui.jsx
@@ -4,35 +4,41 @@ import Grid from "@mui/material/Grid";
 
 const Gridmui = () => {
   return (
-    <Grid container spacing={2}>
-      <Grid
+    <Grid container spacing={3}>
+      {/* <Grid
         container
         direction="row"
         justifyContent="center"
         alignItems="flex-end"
         spacing={1}
       >
+        <CustomDiv width="30%"></CustomDiv>
         <CustomDiv width="30%"> xs=8 </CustomDiv>
         <CustomDiv width="30%"> xs=8 </CustomDiv>
-        <CustomDiv width="30%"> xs=8 </CustomDiv>
+      </Grid> */}
+      <Grid item xs={3}>
+        <CustomDiv width="100%">Artikal 1</CustomDiv>
       </Grid>
-      <Grid item xs={2}>
-        <CustomDiv width="100%"> xs=2 </CustomDiv>
+      <Grid item xs={3}>
+        <CustomDiv width="100%"> Artikal 2 </CustomDiv>
       </Grid>
-      <Grid item xs={2}>
-        <CustomDiv width="100%"> xs=2 </CustomDiv>
+      <Grid item xs={3}>
+        <CustomDiv width="100%"> Artikal 3</CustomDiv>
       </Grid>
-      <Grid item xs={2}>
-        <CustomDiv width="100%"> xs=2 </CustomDiv>
+      <Grid item xs={3}>
+        <CustomDiv width="100%">Artikal 4 </CustomDiv>
       </Grid>
-      <Grid item xs={2}>
-        <CustomDiv width="100%"> xs=2 </CustomDiv>
+      <Grid item xs={3}>
+        <CustomDiv width="100%">Artikal 5</CustomDiv>
       </Grid>
-      <Grid item xs={2}>
-        <CustomDiv width="100%"> xs=2 </CustomDiv>
+      <Grid item xs={3}>
+        <CustomDiv width="100%">Artikal 6</CustomDiv>
       </Grid>
-      <Grid item xs={2}>
-        <CustomDiv width="100%"> xs=2 </CustomDiv>
+      <Grid item xs={3}>
+        <CustomDiv width="100%">Artikal 7</CustomDiv>
+      </Grid>
+      <Grid item xs={3}>
+        <CustomDiv width="100%">Artikal 8</CustomDiv>
       </Grid>
     </Grid>
   );
