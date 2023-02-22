ccd1efbabf9c549fc9317b48cdcda1310be19017
Munevera Plojovic <plojovicm3@gmail.com>
2023-02-23 00:30:56 +0100

    finished main bar for nav

--- Templates/Navigation/MainBar.jsx
+++ Templates/Navigation/MainBar.jsx
@@ -4,29 +4,88 @@ import { getScreenWidth } from "../../util/helpers";
 import { colors, fontSize } from "../../util/theme";
 import Grid from "@mui/material/Grid";
 import Logo from "../../assets/images/logo.png";
+import Text from "../../components/Text/Text";
+import PersonIcon from "@mui/icons-material/Person";
+import StarIcon from "@mui/icons-material/Star";
+import LocationSearchingIcon from "@mui/icons-material/LocationSearching";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import LockIcon from "@mui/icons-material/Lock";
+
+const iconStyle = {
+  fontSize: fontSize.medium,
+  color: colors.gray,
+};
+
+const RightMenu = () => (
+  <CustomDiv
+    display="flex"
+    alignItems="center"
+    justifyContent="flex-end"
+    height="100%"
+  >
+    <CustomDiv display="flex" margin="0px 10px">
+      <PersonIcon style={iconStyle} />
+      <Text fontSize={fontSize.smallPlus} color={colors.gray}>
+        Account
+      </Text>
+    </CustomDiv>
+    <CustomDiv display="flex" margin="0px 10px">
+      <StarIcon style={iconStyle} />
+      <Text fontSize={fontSize.smallPlus} color={colors.gray}>
+        Wishlist
+      </Text>
+    </CustomDiv>
+    <CustomDiv display="flex" margin="0px 10px">
+      <LocationSearchingIcon style={iconStyle} />
+      <Text fontSize={fontSize.smallPlus} color={colors.gray}>
+        Checkout
+      </Text>
+    </CustomDiv>
+    <CustomDiv display="flex" margin="0px 10px">
+      <ShoppingCartIcon style={iconStyle} />
+      <Text fontSize={fontSize.smallPlus} color={colors.gray}>
+        Cart
+      </Text>
+    </CustomDiv>
+    <CustomDiv display="flex" margin="0px 10px">
+      <LockIcon style={iconStyle} />
+      <Text fontSize={fontSize.smallPlus} color={colors.gray}>
+        Login
+      </Text>
+    </CustomDiv>
+  </CustomDiv>
+);
 
 const MainBar = () => {
   return (
     <CustomDiv
-      bgColor={colors.secondColor}
-      display="flex"    
+      bgColor="white"
+      display="flex"
       width="100%"
-      height="30px"
-      padding="0px 5%"
+      height="auto"
+      padding="20px 10% 0  10%"
       border="0px"
     >
       <Grid container direction="row">
         <Grid item md={6} lg={6}>
           <img
             src={Logo}
-            alt="logo"
+            alt="Logo"
             style={{
-              width: 100,
-              height: 50,
+              width: 140,
+              height: "auto",
             }}
           />
         </Grid>
-        <Grid item md={6} lg={6}></Grid>
+        <Grid item md={6} lg={6}>
+          <RightMenu />
+        </Grid>
+        <CustomDiv
+          width="100%"
+          height="2px"
+          margin="20px 0px"
+          borderBottom="0.2px solid #b3b1b1"
+        />
       </Grid>
     </CustomDiv>
   );
