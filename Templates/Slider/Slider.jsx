704aeb8ed411e9b6e00fc327fcae10ac1d1b960d
Munevera Plojovic <plojovicm3@gmail.com>
2023-02-28 12:18:51 +0100

    slider finished

--- Templates/Slider/Slider.jsx
+++ Templates/Slider/Slider.jsx
@@ -3,26 +3,58 @@ import Carousel from "../../components/Carousel/Carousel";
 import girl1 from "../../assets/images/home/girl1.jpg";
 import girl2 from "../../assets/images/home/girl2.jpg";
 import girl3 from "../../assets/images/home/girl3.jpg";
+import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
+import { Grid } from "@mui/material";
+import { getScreenWidth } from "../../util/helpers";
 
 const Slider = () => {
   const sliders = [
     {
       imgSrc: girl1,
-
-      imgText: "Image text1",
+      imgText: "Snaha queen",
     },
     {
       imgSrc: girl2,
-
-      imgText: "Image text 2",
+      imgText: "Svekrvica",
     },
     {
       imgSrc: girl3,
-
-      imgText: "image text 3",
+      imgText: "Zaoovicaa",
     },
   ];
-  return <Carousel data={sliders} />;
+
+  const Slider = () => {
+    const deviceWidth = getScreenWidth();
+
+    const setSliderMargin =
+      deviceWidth === "SM"
+        ? "0px"
+        : deviceWidth === "MD"
+        ? "0px 10px"
+        : "0px 10%";
+
+    const setPaddingTop =
+      deviceWidth === "SM" ? "10px" : deviceWidth === "MD" ? "20px" : "30px";
+  };
+
+  const styles = {
+    sliderContainer: {
+      width: "100%",
+      height: "200px",
+      //paddingTop: setPaddingTop,
+      //margin: setSliderMargin,
+    },
+  };
+
+  return (
+    <Grid container direction="row">
+      <Grid item sm={12} xs={12} md={12} lg={12}>
+        <SimplifiedDiv style={styles.sliderContainer}>
+          <Carousel data={sliders} />
+        </SimplifiedDiv>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Slider;
