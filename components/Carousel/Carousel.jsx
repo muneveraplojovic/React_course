25d252849368c75b466b4f77ab04040cc5fbbeab
Munevera Plojovic <plojovicm3@gmail.com>
2023-02-28 12:20:15 +0100

    carousel functionality added

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -1,13 +1,49 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel as SliderCarousel } from "react-responsive-carousel";
+import Text from "../../components/Text/Text";
+import { colors, fontSize } from "../../util/theme";
+import SimplifiedDiv from "../SimplifiedDiv/SimplifiedDiv";
+//import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+//import ArrowBackNewIcon from "@material-ui/icons/ArrowBackIosNew";
 
 const Carousel = ({ data }) => {
+  const styles = {
+    carouselText: {
+      position: "absolute",
+      bottom: 30,
+      right: 10,
+      left: 10,
+    },
+  };
+
   return (
-    <SliderCarousel>
+    <SliderCarousel
+      width="100%"
+      axis="horizontal"
+      showIndicators
+      showThumbs={false}
+      interval={2000}
+      autoPlay
+      infiniteLoop
+      showStatus={false}
+      showArrows={false}
+      swsipeable
+    >
       {data.map((d) => (
-        <div>
-          <img src={d.imgSrc} />
-          <p className="legend">{d.imgText}</p>
+        <div key={d} style={{ height: "400px" }}>
+          <img
+            src={d.imgSrc}
+            style={{ height: "100%", objectFit: "contain" }}
+          />
+          <SimplifiedDiv style={styles.carouselText}>
+            <Text
+              fontSize={fontSize.xLarge}
+              color={colors.gray}
+              textAlign="left"
+            >
+              {d.imgText}
+            </Text>
+          </SimplifiedDiv>
         </div>
       ))}
     </SliderCarousel>
