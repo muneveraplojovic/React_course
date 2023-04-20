9d593c3158f89131e51de25b41325e42212272be
Munevera Plojovic <plojovicm3@gmail.com>
2023-04-20 23:39:44 +0200

    fonts and some stuff

--- util/theme.js
+++ util/theme.js
@@ -1,23 +1,20 @@
-export const colors = {
-  accentColor: "#e88f23",
-  secondColor: "#F0F0E9",
-  gray: "#828181",
-  lightGray: "#b3b1b1",
-};
-
-export const fontWeight = {
-  light: "100",
-  medium: "400",
-  mediumBold: "600",
-  bold: "800",
-};
-
-export const fontSize = {
-  xSmall: "10px",
-  small: "12px",
-  smallPlus: "14px",
-  normal: "16px",
-  medium: "18px",
-  large: "24px",
-  xLarge: "36px",
+export const fontFamily = {
+  MontserratBlack: "MontserratBlack",
+  MontserratBlackItalic: "MontserratBlackItalic",
+  MontserratBold: "MontserratBold",
+  MontserratBoldItalic: "MontserratBoldItalic",
+  MontserratExtraBold: "MontserratExtraBold",
+  MontserratExtraBoldItalic: "MontserratExtraBoldItalic",
+  MontserratExtraLight: "MontserratExtraLight",
+  MontserratExtraLightItalic: "MontserratExtraLightItalic",
+  MontserratItalic: "MontserratItalic",
+  MontserratLight: "MontserratLight",
+  MontserratLightItalic: "MontserratLightItalic",
+  MontserratMedium: "MontserratMedium",
+  MontserratMediumItalic: "MontserratMediumItalic",
+  MontserratRegular: "MontserratRegular",
+  MontserratSemiBold: "MontserratSemiBold",
+  MontserratSemiBoldItalic: "MontserratSemiBoldItalic",
+  MontserratThin: "MontserratThin",
+  MontserratThinItalic: "MontserratThinItalic",
 };
