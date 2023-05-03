6b4f88d14ccdeb40cabcff41d735c0407da38e70
Munevera Plojovic <plojovicm3@gmail.com>
2023-05-03 20:54:20 +0300

    screens

---
+++ Screens/BlogScreen/BlogScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "../../Templates/Navigation/Navigation";
+import Footer from "../../Templates/Footer/Footer";
+import ItemTable from "../ShopScreen/ItemTable";
+
+const BlogScreen = () => {
+  return <div>
+    <Navigation/>
+    <ItemTable/>
+    <Footer/>
+  </div>;
+};
+
+export default BlogScreen;
---
+++ Screens/ShopScreen/ItemTable.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import CustomDiv from "../../components/customDiv/CustomDiv";
+
+const ItemTable = () => {
+  return (
+    <div style={{
+        width: '100%',
+        height: "100vh",
+        //border: "10px solid black",
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+    }}>
+      <CustomDiv
+         width="80%"
+         height="60%"
+         border="1px solid black"
+        // textAlign="center"
+        // display="flex"
+        // justifyContent="center"
+        // alignItems="center"
+      >
+        <
+      </CustomDiv>
+    </div>
+  );
+};
+
+export default ItemTable;
